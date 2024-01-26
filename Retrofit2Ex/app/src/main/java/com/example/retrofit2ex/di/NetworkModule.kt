d9005aac233fc6a29ea633a086e2dff87b59be30
nala0897@naver.com <nala0897@naver.com>
2024-01-26 10:49:43 +0900

    주석 추가

--- Retrofit2Ex/app/src/main/java/com/example/retrofit2ex/di/NetworkModule.kt
+++ Retrofit2Ex/app/src/main/java/com/example/retrofit2ex/di/NetworkModule.kt
@@ -37,6 +37,16 @@ class NetworkModule {
         return GsonConverterFactory.create()
     }
 
+    /*
+        @Provides 를 붙여서 Retrofit 생성하는 방법을 Hilt에 알리기!
+
+        Hilt는 @Provides 가 달린 provideRetrofit()가
+        * 반환 타입인 Retrofit 를 제공함을 알 수 있다.
+        * 매개변수인 GsonConverterFactory, OkHttpClient 를 통해 provideOkHttpClient(),
+        provideGsonConverterFactory()에 종속되었음을 알 수 있다.
+        * 메소드의 본문은 Retroit을 제공하는 방법을 알려준다.
+        * 따라서 Hilt는 Retrofit를 필요할 때마다 provideRetrofit()를 실행해야 한다.
+    */
     @Provides
     @Singleton
     fun provideRetrofit(gsonConverterFactory: GsonConverterFactory, okHttpClient: OkHttpClient): Retrofit {
