26c6448d4084a6674b4113ad282a3503e4b20d98
nala0897@naver.com <nala0897@naver.com>
2024-01-31 10:24:29 +0900

    Room 을 위한 entity 생성

--- Retrofit2Ex/app/src/main/java/com/example/retrofit2ex/domain/model/UserModel.kt
+++ Retrofit2Ex/app/src/main/java/com/example/retrofit2ex/domain/model/UserModel.kt
@@ -1,8 +1,29 @@
 package com.example.retrofit2ex.domain.model
 
-data class UserModel(
-    val id: Int,
-    val email: String,
-    val name: String,
-    val avatar: String
-)
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "user")
+//data class UserModel(
+//    val id: Int,
+//    val email: String,
+//    val name: String,
+//    val avatar: String
+//)
+
+class UserModel() {
+
+    @PrimaryKey
+    @SerializedName("id")
+    var id: Int = 0
+
+    @SerializedName("email")
+    var email: String = ""
+
+    @SerializedName("name")
+    var name: String = ""
+
+    @SerializedName("avatar")
+    var avatar: String = ""
+}
