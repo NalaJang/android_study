8737a99a819cac3d13131a0e00c831cb7a3ae134
nala0897@naver.com <nala0897@naver.com>
2024-02-01 10:27:34 +0900

    repository 에 UserDao 주입

--- Retrofit2Ex/app/src/main/java/com/example/retrofit2ex/data/repository/UserRepository.kt
+++ Retrofit2Ex/app/src/main/java/com/example/retrofit2ex/data/repository/UserRepository.kt
@@ -1,16 +1,21 @@
 package com.example.retrofit2ex.data.repository
 
 import com.example.retrofit2ex.data.entity.User
+import com.example.retrofit2ex.data.local.UserDao
 import com.example.retrofit2ex.data.source.NetworkService
+import com.example.retrofit2ex.domain.model.UserModel
 import javax.inject.Inject
 
 // 매개변수인 NetworkService 는 UserRepository 의 종속(dependency) 항목이다.
 // 따라서 Hilt 는 NetworkService 를 제공하는 방법도 알고 있어야 한다.
 class UserRepository @Inject constructor(
-    private val networkService: NetworkService
+    private val networkService: NetworkService,
+    private val userDao: UserDao
 ) {
    // page 에 있는 user 데이터를 가지고 온다.
     suspend fun getUserList(page: Int): List<User> {
         return networkService.getUserList(page).data
     }
+
+    suspend fun getUsers(): List<UserModel> = userDao.getAllUser()
 }
