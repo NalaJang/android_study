492930cbfbd00d8e1b78c2907ff2dc1f37406c4d
nala0897@naver.com <nala0897@naver.com>
2024-02-02 10:49:30 +0900

    즐겨찾기를 위한 쿼리 추가

--- Retrofit2Ex/app/src/main/java/com/example/retrofit2ex/data/local/UserDao.kt
+++ Retrofit2Ex/app/src/main/java/com/example/retrofit2ex/data/local/UserDao.kt
@@ -1,6 +1,8 @@
 package com.example.retrofit2ex.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.retrofit2ex.domain.model.UserModel
 
@@ -9,4 +11,7 @@ interface UserDao {
 
     @Query("SELECT * FROM user")
     suspend fun getAllUser(): List<UserModel>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveFavorite(userModel: UserModel)
 }
